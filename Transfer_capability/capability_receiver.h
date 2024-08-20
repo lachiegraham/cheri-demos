dfec5425b39fbd905a53069e6f0a30fbbd9e5a76
Lachlan Graham <u7300179@anu.edu.au>
2024-08-20 10:11:02 +1000

    Sending capabilities between compartments, no sealing.

---
+++ Transfer_capability/capability_receiver.h
@@ -0,0 +1,5 @@
+#include <compartment.h>
+
+
+
+__cheri_compartment("capability_receiver") void transfer_capability(const char* allocation);
---
+++ Transfer_capability/capability_sender.h
@@ -0,0 +1,5 @@
+#include <compartment.h>
+
+
+
+__cheri_compartment("capability_sender") void transfer_back(const char* allocation);
